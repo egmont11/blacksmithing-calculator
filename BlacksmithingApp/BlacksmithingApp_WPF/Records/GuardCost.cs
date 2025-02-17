ad144b9022253885d980289941a33e31184915a8
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2025-02-17 19:44:18 +0100

    Setup of WPF version of the app

---
+++ BlacksmithingApp/BlacksmithingApp_WPF/Records/GuardCost.cs
@@ -0,0 +1,11 @@
+﻿namespace BlacksmithingApp_WinForms;
+
+public record GuardCost(
+    string WeaponPartName,
+    int MetalCost,
+    int OtherMaterialCost,
+    int LvlReq,
+    bool HQMRequired,
+    bool LoyaltyCoinRequired,
+    int PremsCost
+    );
